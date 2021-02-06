40af73923852a86520bafa9d769d335bccb7dc56
AlaaRaedDawood <you@example.com>
2021-02-06 16:29:46 +0200

    fix issue

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';  
 
 const api = axios.create({
-    baseURL:'https://mern-sport.herokuapp.com'
+    baseURL:'https://mern-sport.herokuapp.com/'
 })
 
 export default api;
