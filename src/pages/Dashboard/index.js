1fbb700dd38fb646d2c75bcaceb9463a5ede7068
AlaaRaedDawood <you@example.com>
2021-02-06 14:07:12 +0200

    fixing issue

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -114,6 +114,9 @@ import socketio from 'socket.io-client';
         setEventsRequest(new_Events_Requests);
     }
     const getEvents = async (filter) => {
+        if(!(user && user_id)){
+            history.history.push("/login")
+        }
         if(filter == "myEvents"){
             console.log("allllllllaaaaaaaa " +filter);
             const url = '/dashboardUserId' ;
